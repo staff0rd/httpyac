f91720c24d5298a4f18baec7cb8d3bc16d6c6a0b
Stafford Williams <stafford.williams@gmail.com>
2021-11-18 23:32:03 +1100

    fix version test

--- test/version.test.ts
+++ test/version.test.ts
@@ -1,33 +1,10 @@
 import { cli } from '../src';
-// import * as utils from '../src/utils';
-
-/*
- *  Fails with:
-    TypeError: Cannot read property 'warn' of undefined
-
-        67 |
-        68 | export const log: LogHandler = new Logger({
-        > 69 |   level: LogLevel.warn,
-            |                   ^
-        70 |   noTrace: true,
-        71 | });
-        72 |
-
-        at Object.<anonymous> (src/io/logger.ts:69:19)
-        at Object.<anonymous> (src/io/gotHttpClientFactory.ts:8:1)
-*/
-jest.mock('../src/utils');
-
+import * as utils from '../src/utils/configUtils';
 
 describe('version', () => {
   it('should print version', async () => {
-    /*
-    * Fails with
-    * TypeError: Cannot redefine property: parseJson
-    *     at Function.defineProperty (<anonymous>)
-    */
-    // jest.spyOn(utils, 'parseJson').mockResolvedValue({ version: '1.2.3' });
-    jest.spyOn(console, 'log');
+    jest.spyOn(utils, 'parseJson').mockResolvedValue({ version: '1.2.3' });
+    const spy = jest.spyOn(console, 'info');
 
     await cli.execute([
       '',
@@ -35,6 +12,6 @@ describe('version', () => {
       '--version'
     ]);
 
-    expect(console.log).toHaveBeenCalledWith('httpyac v1.2.3');
+    expect(spy).toHaveBeenCalledWith('httpyac v1.2.3');
   });
 });
